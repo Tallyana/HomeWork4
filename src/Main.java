61a2bb7cb2138860649f3f4ce8bfd55d19fa2abf
Admin <iggla67@yandex.ru>
2022-10-26 01:43:28 +0400

    задание1,2,3 ,урок 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Задание 1");
+
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");}
+
+        int age2 = 17;
+        if (age2 >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");}
+
+        int age1 = 17;
+        if (age1 < 18) {
+            System.out.println("Дождитесь совершеннолетия!");}
+
+            int age3 = 19;
+            if (age3 < 18) {
+                System.out.println("Дождитесь совершеннолетия!");}
+
+        System.out.println("Задание 2");
+
+        int age4 = 7;
+        if (age4 >=7) {
+            System.out.println("Ребенок ходит в школу");}
+
+        int age5 = 18;
+        if (age5 >=18) {
+            System.out.println("С 18 лет проходит обучение в Университете");}
+
+        int age6 = 25;
+        if (age6 >=24) {
+            System.out.println("После 24 лет пора искать работу");}
+
+        System.out.println("Задание 3");
+
+        int obshMesta =120;
+        int sidMesta = 60;
+        int stoachMesta = obshMesta-sidMesta ;
+        if (obshMesta >=120) {
+            System.out.println("В вагоне мест нет!");
+        }
+            if (sidMesta >=60);{
+                System.out.println( "В вагоне  нет сидячих мест!");}
+        if (stoachMesta >=60);{
+            System.out.println( "В вагоне  нет стоячих мест!");}
+
     }
-}
+    }
