275c1057c05e1f57a2f0f83da0b49c97716703b9
Admin <iggla67@yandex.ru>
2022-10-27 02:05:06 +0400

    задание1,2,3,4,5,6,7.8 урок 4

--- src/Main.java
+++ src/Main.java
@@ -8,100 +8,143 @@ public class Main {
 
         int age = 19;
         if (age >= 18) {
-            System.out.println("Поздравляем с совершеннолетием!");}
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
 
         int age2 = 17;
         if (age2 >= 18) {
-            System.out.println("Поздравляем с совершеннолетием!");}
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
 
         int age1 = 17;
         if (age1 < 18) {
-            System.out.println("Дождитесь совершеннолетия!");}
+            System.out.println("Дождитесь совершеннолетия!");
+        }
 
-            int age3 = 19;
-            if (age3 < 18) {
-                System.out.println("Дождитесь совершеннолетия!");}
+        int age3 = 19;
+        if (age3 < 18) {
+            System.out.println("Дождитесь совершеннолетия!");
+        }
 
         System.out.println("Задание 2");
 
         int age4 = 7;
-        if (age4 >=7) {
-            System.out.println("Ребенок ходит в школу");}
+        if (age4 >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        }
 
         int age5 = 18;
-        if (age5 >=18) {
-            System.out.println("С 18 лет проходит обучение в Университете");}
+        if (age5 >= 18) {
+            System.out.println("С 18 лет проходит обучение в Университете");
+        }
 
         int age6 = 25;
-        if (age6 >=24) {
-            System.out.println("После 24 лет пора искать работу");}
+        if (age6 >= 24) {
+            System.out.println("После 24 лет пора искать работу");
+        }
 
         System.out.println("Задание 3");
 
-        int obshMesta =120;
+        int obshMesta = 120;
         int sidMesta = 60;
-        int stoachMesta = obshMesta-sidMesta ;
-        if (obshMesta >=120) {
+        int stoachMesta = obshMesta - sidMesta;
+        if (obshMesta >= 120) {
             System.out.println("В вагоне мест нет!");
         }
-            if (sidMesta >=60);{
-                System.out.println( "В вагоне  нет сидячих мест!");}
-        if (stoachMesta >=60);{
-            System.out.println( "В вагоне  нет стоячих мест!");}
+        if (sidMesta >= 60) ;
+        {
+            System.out.println("В вагоне  нет сидячих мест!");
+        }
+        if (stoachMesta >= 60) ;
+        {
+            System.out.println("В вагоне  нет стоячих мест!");
+        }
 
         System.out.println("ДОМАШНЕЕ ЗАДАНИЕ 2");
 
         System.out.println("Задание 1");
 
-         age = 19;
+        age = 19;
         if (age >= 18) {
             System.out.println("1.Поздравляем с совершеннолетием!");
         } else {
-            System.out.println("1.Дождитесь совершеннолетия!");}
+            System.out.println("1.Дождитесь совершеннолетия!");
+        }
 
-         age2 = 17;
+        age2 = 17;
         if (age2 >= 18) {
             System.out.println("2.Поздравляем с совершеннолетием!");
-    } else {
-        System.out.println("2.Дождитесь совершеннолетия!");}
+        } else {
+            System.out.println("2.Дождитесь совершеннолетия!");
+        }
 
 
         System.out.println("Задание 2");
-         age4 = 7;
-        if (age4 >=7) {
+        age4 = 7;
+        if (age4 >= 7) {
             System.out.println("Ребенок ходит в школу");
         } else {
-            System.out.println("Ребенок не ходит в школу");}
+            System.out.println("Ребенок не ходит в школу");
+        }
 
         age5 = 18;
-        if (age5 >=18) {
+        if (age5 >= 18) {
             System.out.println("С 18 лет проходит обучение в Университете");
-        } else  {
+        } else {
             System.out.println("До 18 лет проходит обучение в школе");
         }
 
-          int age9 = 23;
-        if (age9 >=24) {
+        int age9 = 23;
+        if (age9 >= 24) {
             System.out.println("После 24 лет пора искать работу");
-    } else  {
-        System.out.println("До 24 лет проходит обучение в университете");
-    }
+        } else {
+            System.out.println("До 24 лет проходит обучение в университете");
+        }
         System.out.println("Задание 3");
 
-         obshMesta =120;
-         sidMesta = 60;
-         stoachMesta = obshMesta-sidMesta ;
+        obshMesta = 120;
+        sidMesta = 60;
+        stoachMesta = obshMesta - sidMesta;
         if (obshMesta < 120) {
             System.out.println("В вагоне мест нет!");
         } else {
-            System.out.println( "В вагоне еще есть места!");}
+            System.out.println("В вагоне еще есть места!");
+        }
 
-        obshMesta =120;
+        obshMesta = 120;
         sidMesta = 60;
-        stoachMesta = obshMesta-sidMesta ;
+        stoachMesta = obshMesta - sidMesta;
         if (obshMesta >= 120) {
             System.out.println("В вагоне мест нет!");
         } else {
-            System.out.println( "В вагоне еще есть места!");}
-    }
-    }
+            System.out.println("В вагоне еще есть места!");
+        }
+
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ 3");
+
+        System.out.println("Задание 1");
+
+        int age11 = 4;
+        if (age >= 2 && age <= 6);
+        {
+            System.out.println("Если человеку от 2 до 6 лет, то ему нужно ходить в детский сад");
+
+          }
+        int age12 = 11;
+        if ( age >= 7 && age <= 18 );
+        {
+            System.out.println("Если человек от 7 до 18 лет, то ему нужно ходить в школу");
+
+
+            System.out.println("Задание 2");
+
+            age4 = 3;
+            if ( age >= 5 || age <= 14 );
+            System.out.println("Без взрослого кататься нельзя!");
+        }
+
+        System.out.println("Задание 2");
+
+
+      }
+}
