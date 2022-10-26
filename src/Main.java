6171c1909e5273c81fe701e59b27b75799d2965c
Admin <iggla67@yandex.ru>
2022-10-27 00:19:35 +0400

    задание1,2,3,4,5,6,урок 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ 1");
+
         System.out.println("Задание 1");
 
         int age = 19;
@@ -47,5 +49,59 @@ public class Main {
         if (stoachMesta >=60);{
             System.out.println( "В вагоне  нет стоячих мест!");}
 
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ 2");
+
+        System.out.println("Задание 1");
+
+         age = 19;
+        if (age >= 18) {
+            System.out.println("1.Поздравляем с совершеннолетием!");
+        } else {
+            System.out.println("1.Дождитесь совершеннолетия!");}
+
+         age2 = 17;
+        if (age2 >= 18) {
+            System.out.println("2.Поздравляем с совершеннолетием!");
+    } else {
+        System.out.println("2.Дождитесь совершеннолетия!");}
+
+
+        System.out.println("Задание 2");
+         age4 = 7;
+        if (age4 >=7) {
+            System.out.println("Ребенок ходит в школу");
+        } else {
+            System.out.println("Ребенок не ходит в школу");}
+
+        age5 = 18;
+        if (age5 >=18) {
+            System.out.println("С 18 лет проходит обучение в Университете");
+        } else  {
+            System.out.println("До 18 лет проходит обучение в школе");
+        }
+
+          int age9 = 23;
+        if (age9 >=24) {
+            System.out.println("После 24 лет пора искать работу");
+    } else  {
+        System.out.println("До 24 лет проходит обучение в университете");
+    }
+        System.out.println("Задание 3");
+
+         obshMesta =120;
+         sidMesta = 60;
+         stoachMesta = obshMesta-sidMesta ;
+        if (obshMesta < 120) {
+            System.out.println("В вагоне мест нет!");
+        } else {
+            System.out.println( "В вагоне еще есть места!");}
+
+        obshMesta =120;
+        sidMesta = 60;
+        stoachMesta = obshMesta-sidMesta ;
+        if (obshMesta >= 120) {
+            System.out.println("В вагоне мест нет!");
+        } else {
+            System.out.println( "В вагоне еще есть места!");}
     }
     }
