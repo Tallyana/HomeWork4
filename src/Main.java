5c301af52dfab5faacbc6ee0198bddf09c572784
Admin <iggla67@yandex.ru>
2022-10-27 20:33:49 +0400

    задание1,2,3,4,5,6,7.8 урок 4

--- src/Main.java
+++ src/Main.java
@@ -125,13 +125,13 @@ public class Main {
         System.out.println("Задание 1");
 
         int age11 = 4;
-        if (age >= 2 && age <= 6);
+        if (age >= 2 && age <= 6) ;
         {
             System.out.println("Если человеку от 2 до 6 лет, то ему нужно ходить в детский сад");
 
-          }
+        }
         int age12 = 11;
-        if ( age >= 7 && age <= 18 );
+        if (age >= 7 && age <= 18) ;
         {
             System.out.println("Если человек от 7 до 18 лет, то ему нужно ходить в школу");
 
@@ -139,12 +139,22 @@ public class Main {
             System.out.println("Задание 2");
 
             age4 = 3;
-            if ( age >= 5 || age <= 14 );
+            if (age >= 5 || age <= 14) ;
             System.out.println("Без взрослого кататься нельзя!");
         }
 
-        System.out.println("Задание 2");
+        System.out.println("Задание 3");
 
-
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        boolean formulaOne = one > three && two < three;
+     if (formulaOne) {
+        System.out.println("Условия выполнены");
+    } else {
+        System.out.println("Три больше 1 и 2");
       }
+
+
+}
 }
